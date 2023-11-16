db2ead1d48a23aed5a0292931c83944eba672d80
seaerchin <jiachin@open.gov.sg>
2023-11-16 17:10:23 +0800

    feat(tokens): add tokens and printing stuff

--- rox/src/rox/mod.rs
+++ rox/src/rox/mod.rs
@@ -1,3 +1,4 @@
 pub mod rox;
 pub mod scanner;
 pub mod token;
+pub mod token_type;
--- rox/src/rox/token.rs
+++ rox/src/rox/token.rs
@@ -1,1 +1,31 @@
-pub type Token = String;
+use std::fmt::{self, Display};
+
+use super::token_type::TokenType;
+
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub literal: String, // NOTE: original is `Object`
+    pub line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: String, line: usize) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{:?} {} {}", self.token_type, self.lexeme, self.literal)
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
