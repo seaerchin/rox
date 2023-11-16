1f37f4e5ad0f3ea9176d0809e96daef200371f9b
seaerchin <jiachin@open.gov.sg>
2023-11-16 15:57:36 +0800

    feat(runfile): add initial impl

--- rox/src/rox/rox.rs
+++ rox/src/rox/rox.rs
@@ -1,3 +1,19 @@
-pub fn run_file(filename: &String) {}
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+pub fn run_file(filename: &String) -> () {
+    // get path
+    // read file as bytes
+    // run the string
+    let path = Path::new(filename);
+    let buf = &mut Vec::new();
+    let _ = File::open(path).expect("file not found").read_to_end(buf);
+    run(buf);
+}
 
 pub fn run_prompt() {}
+
+fn run(file: &Vec<u8>) -> () {
+    todo!()
+}
